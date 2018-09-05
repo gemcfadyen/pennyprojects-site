79c743a4f8df8892e6ac8ce4909d458facf03035
gemcfadyen <gemcfadyen@hotmail.co.uk>
2018-09-05 10:04:41 +0100

    Styled the footer page using styled-components

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,15 +1,52 @@
 import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.section`
+  padding: 4em;
+    background: papayawhip;
+    `;
+
+const StyledFooterDiv= styled.div`
+   margin: "1rem auto";
+   maxWidth: 600
+`;
+
+const ListWithoutBullet = styled.ul`
+   list-style-type: none;
+`;
+
+const SizedImage = styled.img`
+  height: 75%;
+  width: 20%;
+`;
+
+const PaddedLink = styled.a`
+  padding-right: 5px;
+`;
+
+const StyledListItem = styled.li`
+  display: flex;
+`;
 
 export default ({ children }) => (
-    <div style={{ margin: "1rem auto", maxWidth: 600 }}>
-    <ul class="sociallist">
-      <li class="socialapp">
-        <a href="https://www.github.com/gemcfadyen"> <img src="github.png" alt="github logo" /> </a>
+  <Wrapper>
+    <StyledFooterDiv>
+      <ListWithoutBullet>
+        <StyledListItem>
+          <PaddedLink href="https://www.github.com/gemcfadyen">
+            <SizedImage src="github.png" alt="github logo"/>
+          </PaddedLink>
 
-        <a href="https://www.twitter.com/gemcfadyen"> <img src="twitter.png" alt="twitter logo" /> </a>
+          <PaddedLink href="https://www.twitter.com/gemcfadyen">
 
-        <a href="https://uk.linkedin.com/in/georgina-mcfadyen-03942b104"> <img src="linkedin.png" alt="linkedin logo" /> </a>
-      </li>
-    </ul>
-  </div>
+            <SizedImage src="twitter.png" alt="twitterlogo"/>
+          </PaddedLink>
+
+          <PaddedLink href="https://uk.linkedin.com/in/georgina-mcfadyen-03942b104">
+            <SizedImage src="linkedin.png" alt="linkedin logo"/>
+          </PaddedLink>
+        </StyledListItem>
+      </ListWithoutBullet>
+    </StyledFooterDiv>
+  </Wrapper>
 );
