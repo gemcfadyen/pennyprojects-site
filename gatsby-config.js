7d1d79446598f352d432a13eac6c22436fae7cc1
gemcfadyen <gemcfadyen@hotmail.co.uk>
2018-09-06 19:00:49 +0100

    updating dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,13 +2,6 @@ module.exports = {
   plugins: [
     `gatsby-plugin-styled-components`,
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/src`,
-        name: "all-pages"
-      }
-    },
-    {
       pathPrefix: `/gatsby-spike`,
       resolve: `gatsby-plugin-typography`,
       options: {
