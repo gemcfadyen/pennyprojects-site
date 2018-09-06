40c98426eee883f4b0448ffec50b9b21d69e4bca
gemcfadyen <gemcfadyen@hotmail.co.uk>
2018-09-06 10:08:36 +0100

    Embed videos for the talks page

--- src/pages/talks.js
+++ src/pages/talks.js
@@ -4,7 +4,21 @@ import Container from "../components/container";
 
 export default () => (
   <Container>
-    <h1>Public Appearances</h1>
-    Coming Soon...
+
+    <h1> Under The Elixir Umbrella </h1>
+    <iframe width="420" height="315"
+      src="https://www.youtube.com/embed/jhZwQ1LTdUI">
+    </iframe>
+
+    <h1> SOLID Elixir </h1>
+    <iframe width="420" height="315"
+      src="https://www.youtube.com/embed/eldYot7uxUc&t=98s">
+    </iframe>
+
+    <h1> Elixir: A Mini Documentary </h1>
+
+    <iframe width="420" height="315"
+      src="http://doc.honeypot.io/elixir-documentary-2018/">
+    </iframe>
   </Container>
 );
