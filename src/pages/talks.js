2a50d12c7aff335d00b96688b95ab005efe68693
gemcfadyen <gemcfadyen@hotmail.co.uk>
2018-09-06 10:50:20 +0100

    Adding all talks/workshops

--- src/pages/talks.js
+++ src/pages/talks.js
@@ -4,21 +4,78 @@ import Container from "../components/container";
 
 export default () => (
   <Container>
+    Over the last few years I have made several public apperances in the form of talks and workshops. Here are details on sessions I've held in the Software Community all over Europe:
 
-    <h1> Under The Elixir Umbrella </h1>
-    <iframe width="420" height="315"
-      src="https://www.youtube.com/embed/jhZwQ1LTdUI">
-    </iframe>
+    <p/>
 
     <h1> SOLID Elixir </h1>
+    This talk was presented in Warsaw, Poland in April 2018 as part of the Elixir EU conference.
     <iframe width="420" height="315"
       src="https://www.youtube.com/embed/eldYot7uxUc&t=98s">
     </iframe>
 
     <h1> Elixir: A Mini Documentary </h1>
+    This documentary uses footage from the 2018 Elixir EU Conference held in Warsaw where I was interviewed and feature around 9 minutes in.
 
     <iframe width="420" height="315"
       src="http://doc.honeypot.io/elixir-documentary-2018/">
+    </iframe>
+
+    <h1> SOLID Elixir Keynote </h1>
+This talk was presented as the closing keynote speaker in  Milan, Italy in April 2018 as part of the CodeBeam Lite conference.
+    <iframe width="420" height="315"
+      src="https://codesync.global/speaker/georgina-mcfadyen">
+    </iframe>
+
+    <h1> Don't Diss The Discipline </h1>
+This talk was given in Swansea, Wales as part of SwanseaCon.
+    <iframe width="420" height="315"
+      src="http://swanseacon.co.uk/archive2017/">
+    </iframe>
+    http://swanseacon.co.uk/archive2017/
+
+    <h1> Under The Elixir Umbrella </h1>
+    This talk took place as part of the Elixir LDN conference in August 2017.
+
+    <iframe width="420" height="315"
+      src="https://www.youtube.com/embed/jhZwQ1LTdUI">
+    </iframe>
+
+    <h1> Microservices Under the Umbrella </h1>
+        This workshop was held in Barcelona, Spain as part of Elixir EU in May 2017.
+
+    <iframe width="420" height="315"
+      src="http://www.elixirconf.eu/elixirconf2017/georgina-mcfadyen">
+    </iframe>
+
+    <h1>Introduction to Elixir</h1>
+This introduction to Elixir Umbrellas was given at the Ladies of Code meetup in London, April 2017.
+    <iframe width="420" height="315"
+      src="https://www.meetup.com/Ladies-of-Code-UK/events/236922126/">
+    </iframe>
+
+    <h1> 8LU Legacy Code Retreat </h1>
+This workshop was given in February 2017 as part of 8th Light University in London.
+    <iframe width="420" height="315"
+      src="https://www.meetup.com/8LU-London/events/237311782/">
+    </iframe>
+
+    <h1> Extract Your Own Test Framework </h1>
+This workshop was given in December 2016 at London Software Craftsmanship Community.
+    <iframe width="420" height="315"
+      src="https://www.meetup.com/london-software-craftsmanship/events/235776594/">
+    </iframe>
+
+    <h1> TDD From the inside out or the outside in</h1>
+This talk discussed two approches of TDD. It was given as part of the Ladies of Code meetup group in London, August 2016.
+    <iframe width="420" height="315"
+      src="https://www.meetup.com/Ladies-of-Code-UK/events/232609419/">
+    </iframe>
+
+    <h1> London Techonology Week: Diversity in Technology</h1>
+    As part of London Technology week, a presentation was given on the current diversity issues that the software industry faces, July 2016.
+    <iframe width="420" height="315"
+      src="https://getstarted.ticketmaster.co.uk/news/london-technology-week-wrap">
     </iframe>
   </Container>
 );
