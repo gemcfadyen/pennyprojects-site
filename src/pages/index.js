6752e7521aabd2b02648dc656a333848a61db950
gemcfadyen <gemcfadyen@hotmail.co.uk>
2019-07-05 08:58:51 +0100

    removing wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Container from "../components/container";
+import CardBase from "../components/cards/base/base";
+import CardHeader from "../components/cards/header/header";
+import CardContent from "../components/cards/content/content";
 import styled from "styled-components";
 
 const CircularImage = styled.img`
@@ -25,13 +28,17 @@ const BoldText = styled.p`
 
 export default () => (
   <Container>
-    <CircularImage src="headshot.jpeg" alt="" />
+    <CircularImage src="headshot.jpeg" alt=""/>
 
-    <p>I'm Georgina, a Contract Polyglot Developer. I enjoy working on high performing, collaborative Agile teams where challenges are embraced, modern technologies applied, and software delivered using clean coding techniques and best practices.</p>
-    <MainTitle>Summary of Technologies: </MainTitle> Polyglot Full Stack Developer: Java, JavaScript, React, Ruby, Elixir, PHP, C++, SQL, Gigaspaces, Spring Batch Framework, Spring, Hibernate, Xslt, Maven, Gradle, Git.
+    <p>I'm Georgina, a Contract Polyglot Developer. I enjoy working on high performing, collaborative Agile teams where
+      challenges are embraced, modern technologies applied, and software delivered using clean coding techniques and
+      best practices.</p>
+    <MainTitle>Summary of Technologies: </MainTitle> Polyglot Full Stack Developer: Java, JavaScript, React, Ruby,
+    Elixir, PHP, C++, SQL, Gigaspaces, Spring Batch Framework, Spring, Hibernate, Xslt, Maven, Gradle, Git.
 
-  <p/>
+    <p/>
 
-  <MainTitle>Summary of Methodologies: </MainTitle> Scrum, Kanban, Test Driven Development, Behaviour Driven Development, Specification by Example, Pair-programming.
-</Container>
+    <MainTitle>Summary of Methodologies: </MainTitle> Scrum, Kanban, Test Driven Development, Behaviour Driven
+    Development, Specification by Example, Pair-programming.
+  </Container>
 );
